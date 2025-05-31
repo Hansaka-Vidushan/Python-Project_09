22ed5e835488ae60d4295cc9e8314f9bb71f05ea
Hansaka-Vidushan <hansakavidushan56@gmail.com>
2025-05-31 11:09:59 +0530

    Explain Formatting

---
+++ Formatting.py
@@ -0,0 +1,24 @@
+# Get pyformat.info
+
+name = "Hansaka"
+age = 19
+na = "Hello %s. Your age is %d" %(name,age)
+
+# We can use index for string formatting
+ma = "Hello {1}. Your age is {0}".format(name,age)
+
+# Using "f"
+sa = f"Hello {name}. Your name is {age}"
+
+print(na)
+print(ma)
+print(sa)
+
+
+# String = %s
+# int = %d
+# float = %f
+
+
+
+
--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Python-Project_09
 # Hansaka Vidushan
 # InnoCodeLK
+# Formatting
+# pyformat.info
+# string formatting
 
 This is my 09 python project
+
+Day 18
